41fc29b537ad2603de22b53dc5123628012e44f9
ulysses <faustolopez110@gmail.com>
2019-10-30 21:25:23 -0400

    Updates

---
+++ precache-manifest.cbd3ebd2d6540f7492beef7b17ab3eaf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59a9e28d3adf6d8df642ad2eba560470",
+    "url": "/taxi-indicators-dash/index.html"
+  },
+  {
+    "revision": "1636f00234c072d6ca5a",
+    "url": "/taxi-indicators-dash/static/css/main.fdbf123e.chunk.css"
+  },
+  {
+    "revision": "a5258e5b6e44893209c9",
+    "url": "/taxi-indicators-dash/static/js/2.edd98bed.chunk.js"
+  },
+  {
+    "revision": "1636f00234c072d6ca5a",
+    "url": "/taxi-indicators-dash/static/js/main.eeebc612.chunk.js"
+  },
+  {
+    "revision": "89d2955cff54c12f3baa",
+    "url": "/taxi-indicators-dash/static/js/runtime-main.c607ddf5.js"
+  }
+]);
